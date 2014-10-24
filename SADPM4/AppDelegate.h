4f156692f50bd16f6e15fb9b6be7c64f57b7334a
kawakamiyuhei <kawakami@db.soc.i.kyoto-u.ac.jp>
2014-10-24 14:20:33 +0900

    Initial Commit

---
+++ SADPM4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SADPM4
+//
+//  Created by kawakami_mac on 2014/10/24.
+//  Copyright (c) 2014年 kawakami_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SADPM4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SADPM4
+//
+//  Created by kawakami_mac on 2014/10/24.
+//  Copyright (c) 2014年 kawakami_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
